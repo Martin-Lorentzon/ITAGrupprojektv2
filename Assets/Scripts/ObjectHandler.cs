4ab4e0073fe138916162d66d579ce8d6bd322f1d
Martin Lorentzon <106841201+Martin-Lorentzon@users.noreply.github.com>
2023-04-04 14:23:11 +0200

    Added functionality to de-select objects
    ...and changed "add to selection" to shift

---
+++ Assets/Scripts/ObjectHandler.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ObjectHandler : MonoBehaviour
+{
+
+    public List<GameObject> selectedObjects;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Input.GetKey(KeyCode.LeftShift))            // Add to selection
+                {
+                    if (hit.transform.tag != "Immovable")
+                    {
+                        if (selectedObjects.Contains(hit.transform.gameObject))
+                            selectedObjects.Remove(hit.transform.gameObject);
+                        else
+                            selectedObjects.Add(hit.transform.gameObject);
+                    }
+                }
+                else
+                {
+                    if (hit.transform.tag != "Immovable")       // Select single object
+                    {
+                        if (selectedObjects.Contains(hit.transform.gameObject))
+                            selectedObjects.Remove(hit.transform.gameObject);
+                        else
+                        {
+                            selectedObjects.Clear();
+                            selectedObjects.Add(hit.transform.gameObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
