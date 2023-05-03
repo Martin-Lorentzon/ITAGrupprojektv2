7e57938953b023d38eaad7320514c53570680dd3
OnHike <elmeras29@gmail.com>
2023-05-03 17:48:11 +0200

    object fix

--- Assets/Scripts/MeshLibrary.cs
+++ Assets/Scripts/MeshLibrary.cs
@@ -9,16 +9,15 @@ using UnityEngine.UI;
 public class MeshLibrary : MonoBehaviour
 {
 
-    public GameObject meshContainer;
-    public List<Mesh> meshList;
-    public MeshFilter meshFilter;
-    private float originPointOffset;
-    public List<Texture2D> thumbnails;
+    //public GameObject meshContainer;
+    //public List<Mesh> meshList;
+    //public MeshFilter meshFilter;
+    //public List<Texture2D> thumbnails;
     public List<GameObject> buttons;
-    public List<Sprite> sprites;
-    public GameObject button;
-    private Vector3 pos;
-    public GameObject UIpanel;
+    //public List<Sprite> sprites;
+    //public GameObject button;
+    ////private Vector3 pos;
+    //public GameObject UIcontentContainer;
     public Camera cameraObj;
     public List<GameObject> prefabs;
     GameObject prefab;
@@ -26,15 +25,20 @@ public class MeshLibrary : MonoBehaviour
     GameObject instance;
     public Material glassMat;
     int sceneAssetLayer;
+    int groundLayer;
+    LayerMask noHitLayer;
     void Start()
     {
 
         prefab = prefabs[0];
-        meshFilter = prefabs[0].GetComponent<MeshFilter>();
-        originPointOffset = meshFilter.sharedMesh.bounds.size.y / 2;
-        pos = (button.GetComponent<RectTransform>().position);
-        SetThumbnail();
+        //meshFilter = prefabs[0].GetComponent<MeshFilter>();
+
+        //pos = (button.GetComponent<RectTransform>().position);
+        //SetThumbnail();
         sceneAssetLayer = LayerMask.NameToLayer("Scene Asset");
+        groundLayer = LayerMask.GetMask("Ground");
+        noHitLayer = LayerMask.GetMask("UI" , "Ground");
+
 
     }
 
@@ -46,19 +50,32 @@ public class MeshLibrary : MonoBehaviour
         // offsets y position by half the mesh size, so that the object is placed on the planes surface.
         // sets instance to be on scene asset layer
 
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
+
+
+            //if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
             Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePosition);
 
+
+
             RaycastHit hit;
-            Physics.Raycast(cameraObj.transform.position, (mouseWorldPos - cameraObj.transform.position).normalized, out hit, 50f);
+            Physics.Raycast(cameraObj.transform.position, (mouseWorldPos - cameraObj.transform.position).normalized, out hit, 500f);
+            //if (Physics.Raycast(Camera.main.ScreenPointToRay(mousePosition), 10000f, ~noHitLayer))
+
+            //    Debug.Log("hit non-dirt");
+            //return;
+
+           
             if (hit.collider != null)
             {
-                if (hit.collider.tag == "Plane")
+
+                Debug.Log(hit.point);
+                if (hit.collider.tag == "Plane" && UILayerCheck.uiHover == false)
                 {
 
-                    instance = Instantiate(prefab, hit.point + new Vector3(0, originPointOffset, 0), prefab.transform.rotation);
+                    instance = Instantiate(prefab, hit.point, prefab.transform.rotation);
                     SetMaterial();
                     instance.layer = sceneAssetLayer;
                     prefab.AddComponent<DontDestroyOnLoad>();
@@ -69,28 +86,29 @@ public class MeshLibrary : MonoBehaviour
 
     }
 
-    
+
     // Körs vid start, hämtar asset preview thumbnails för objekt och sätter som sprites för knapparna.
-    public void SetThumbnail()
-    {
-        for (int i = 0; i < prefabs.Count; i++)
-        {
-            GameObject buttonInstance = Instantiate(button);
-            buttons.Add(buttonInstance);
-            buttonInstance.GetComponent<RectTransform>().position = pos;
+    //public void SetThumbnail()
+    //{
+    //    for (int i = 0; i < prefabs.Count; i++)
+    //    {
+    //        GameObject buttonInstance = Instantiate(button);
+    //        buttons.Add(buttonInstance);
+    //        buttonInstance.GetComponent<RectTransform>().position = pos;
 
-            //thumbnails.Add(null);
-            //sprites.Add(null);
-            //thumbnails[i] = AssetPreview.GetAssetPreview(prefabs[i]);
-            //sprites[i] = Sprite.Create(thumbnails[i], new Rect(0f, 0f, 128f, 128f), new Vector2(0f, 0f));
-            //buttons[i].GetComponent<Image>().sprite = sprites[i];
+    //        thumbnails.Add(null);
+    //        sprites.Add(null);
+    //        thumbnails[i] = AssetPreview.GetAssetPreview(prefabs[i]);
+    //        sprites[i] = Sprite.Create(thumbnails[i], new Rect(0f, 0f, 50f, 50f), new Vector2(0f, 0f));
+    //        buttons[i].GetComponent<Image>().sprite = sprites[i];
 
-            pos = pos + new Vector3(70, 0, 0);
-            buttonInstance.transform.SetParent(UIpanel.transform);
+    //        pos = pos + new Vector3(70, 0, 0);
+    //        buttonInstance.transform.SetParent(UIcontentContainer.transform);
 
-        }
-    }
-    
+    //    }
+    //    Destroy(button);
+    //}
+
 
     // sätter material vid instansiering. Sätter material med namnet glass till ett eget material
     public void SetMaterial()
@@ -131,9 +149,12 @@ public class MeshLibrary : MonoBehaviour
     public void ChangeMesh(int idx)
     {
         prefab = prefabs[idx];
-        originPointOffset = prefab.GetComponent<MeshFilter>().sharedMesh.bounds.size.y / 2;
+        SceneInformation.selectedObjects.Clear();
+        FindAnyObjectByType<ObjectHandler>().ClearSelected();
+        //originPointOffset = prefab.GetComponent<MeshFilter>().sharedMesh.bounds.size.y / 2;
 
     }
 
 
 }
+
